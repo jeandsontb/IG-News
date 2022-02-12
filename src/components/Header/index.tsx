51d03bc4f5fa7f26ddb63a0a1fa8f92a18d58843
jeandson <jeandsontb@gmail.com>
2022-02-12 17:09:07 -0300

    include project

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <img src="/images/logo.svg" alt="Ig.News" />
+        
+      </div>
+    </header>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ig.News</title>
+      </Head>
+      <h1>hello word</h1>
+    </>
+  )
+}
