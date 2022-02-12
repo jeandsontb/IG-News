007e6f25309755ca8f9019c5dad99049b2277a4e
jeandson <jeandsontb@gmail.com>
2022-02-12 19:46:41 -0300

    update4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from "next/head"
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from '../services/stripe';
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1KSTO5AvF4r6gu2Izbjll0cl');
 
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 horas
   }
 }
